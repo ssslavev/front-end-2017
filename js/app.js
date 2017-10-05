b029e66c869556efe62490d910742e56524d6d11
s.s.slavev <s.s.slavev@hotmail.com>
2017-10-06 00:39:31 +0300

    Added carousel and home template

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-$('body').on('hover', '.dropdown', function() {
-    $('.dropdown-menu').show();
-})
+(function() {
+
+    let router = new Navigo(null, false);
+
+
+    router
+        .on({
+
+            '/home': () => templates.get("home").then((tmpl) => $('#main-content').html(tmpl)),
+            '/post': () => templates.get("post").then((tmpl) => $('#main-content').html(tmpl)),
+            '/sign': () => templates.get("login").then((tmpl) => $('#main-content').html(tmpl)),
+            '/register': () => templates.get("register").then((tmpl) => $('#main-content').html(tmpl))
+
+
+
+        })
+        .resolve();
+
+
+
+
+
+}());
