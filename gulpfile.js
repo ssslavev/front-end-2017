257307064c08dcee054908e68bf48d136d09519a
s.s.slavev <s.s.slavev@hotmail.com>
2017-10-10 22:06:05 +0300

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,42 @@
 const gulp = require("gulp");
-const clean = require("gulp-clean");
+const del = require("del");
+const eslint = require("gulp-eslint");
 const uglify = require("gulp-uglify");
 const pump = require("pump");
 const babel = require("gulp-babel");
+const cleanCSS = require("gulp-clean-css");
+const gulpsync = require("gulp-sync")(gulp);
 
+
+//Clean build directory
 gulp.task("clean", () => {
-    gulp.src("build", { read: false })
-        .pipe(clean());
+    return del("build");
 })
 
-gulp.task("test", () => {
-    console.log("test");
-})
-
+//Move files to build directory
 gulp.task("move", () => {
-    gulp.src(['*controllers/*.*'])
+    gulp.src(['*images/*.*', 'index.html', 'package.json', '*templates/*.*'])
         .pipe(gulp.dest("build"))
 })
 
+
+//Uglify js and css
 gulp.task("uglify:js", () => {
 
-    gulp.src("js/*.js")
+    gulp.src(["*js/*.js", "*controllers/*.js"])
         .pipe(babel({
             presets: ['env']
         }))
         .pipe(uglify())
         .pipe(gulp.dest("build"))
-        .pipe(gulp.dest('build/js'))
-
 })
+
+gulp.task('uglify:css', () => {
+    return gulp.src('styles.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('uglify', ['uglify:js', 'uglify:css']);
+
+gulp.task('build', gulpsync.sync(['clean', 'move', 'uglify']));
