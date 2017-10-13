06537ca6b383b05da298d158f6e20d83b5704375
s.s.slavev <s.s.slavev@hotmail.com>
2017-10-13 17:53:21 +0300

    Added new main slider

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,17 +3,21 @@ let homeController = (function() {
     function getHomePage(context) {
 
         let recipes = [];
+        let sliderRecipes;
+
 
         return data.recipes.getLimitRecipes(10)
             .then((reqRecipes) => {
 
                 recipes = reqRecipes;
+                sliderRecipes = reqRecipes.slice(0, 7);
+                console.log(sliderRecipes);
 
                 return templates.get('home');
             })
             .then((tmpl) => {
-                context.$element().html(tmpl({ recipes: recipes }));
-
+                context.$element().html(tmpl({ recipes: recipes, sliderRecipes: sliderRecipes }));
+                slider.slide();
             })
     }
 
